c806a950bd23476a92c8d2134072eacd26fe2d1c
Brandon <estanob@gmail.com>
2021-08-06 14:26:38 -0700

    Fix canvas height scrolling issue interfering with game play

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const InfiniteRunner = require('../lib/game.js');
 
-const canvas = document.getElementById('game-canvas');
+const canvas = document.getElementById('game-canvas').setAttribute('width', '3000px').setAttribute('height', '5000px');
 new InfiniteRunner(canvas);
