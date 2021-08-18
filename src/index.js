7cb79c2dd743ef0fa59cad9cea8f42797049e7d6
Brandon <estanob@gmail.com>
2021-08-18 13:37:00 -0700

    Fix game canvas height problem

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const InfiniteRunner = require('../lib/game.js');
+const Game = require('../lib/game.js');
 
-const canvas = document.getElementById('game-canvas').setAttribute('width', '3000px').setAttribute('height', '5000px');
-new InfiniteRunner(canvas);
+
+const canvas = document.getElementById('game-canvas').setAttribute('width', '1500px').setAttribute('height', '650px');
+// const canvas = document.getElementById('game-canvas').setAttribute('width', '3000px').setAttribute('height', '5000px');
+new Game(canvas);
+
+const pause = document.getElementById("pause-button");
+pause.addEventListener("click", canvas.pauseGame);
