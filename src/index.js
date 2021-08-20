9f75f83bd41d4f4d7839b3ee6740c283c4b65a66
Brandon <estanob@gmail.com>
2021-08-20 13:54:20 -0700

    Fix game styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const Game = require('../lib/game.js');
 
 
-const canvas = document.getElementById('game-canvas').setAttribute('width', '1500px').setAttribute('height', '650px');
+const canvas = document.getElementById('game-canvas').setAttribute('width', '900px').setAttribute('height', '650px');
+// const canvas = document.getElementById('game-canvas').setAttribute('width', '1500px').setAttribute('height', '650px');
 // const canvas = document.getElementById('game-canvas').setAttribute('width', '3000px').setAttribute('height', '5000px');
 new Game(canvas);
 
